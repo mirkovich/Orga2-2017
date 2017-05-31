b0f553eb3a62d565da057b6915a6ad0e04c066e4
franketi01 <francogm01@gmail.com>
2017-05-31 15:41:10 -0300

    Add files via upload
    recorre desde arriba

--- TP2/entregable/tp2-bundle.v2/src/filters/C_linearZoom.c
+++ TP2/entregable/tp2-bundle.v2/src/filters/C_linearZoom.c
@@ -14,147 +14,155 @@
 #include <sys/time.h>
 
 
-
-
-
-uint8_t saturation(uint8_t num1, uint8_t num2){
-	if(num1 < num2){
-		 return num1;
-		}
-	return num2; 
-	}
-
-double timeval_diff(struct timeval *a, struct timeval *b)
-{
-  return
-    (double)(a->tv_sec + (double)a->tv_usec/1000000) -
-    (double)(b->tv_sec + (double)b->tv_usec/1000000);
-}
-
-
-
-
+uint8_t suma2(uint8_t pa, uint8_t pb);
+uint8_t suma4(uint8_t pa, uint8_t pb, uint8_t pc, uint8_t pd);
 
 void C_linearZoom(uint8_t* src, uint32_t srcw, uint32_t srch,
                   uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused))) {
 				
-		struct timeval t_ini, t_fin;
-  		double secs;
 
- 	 	gettimeofday(&t_ini, NULL);
-
-		uint8_t (*src_matriz)[srcw * 4] = (uint8_t (*)[srcw * 4])src; 
-		uint8_t (*dst_matriz)[dstw * 4] = (uint8_t (*)[dstw * 4])dst; 
 		
-		int i,j;					   
+		RGBA (*matrix_src)[srcw] = (RGBA (*)[srcw]) src;
+		RGBA (*matrix_dst)[dstw] = (RGBA (*)[dstw]) dst;
+	//	src[fila][col]
+		uint32_t i,j;					   
 
 // completo los pixeles que no tienen que ser interpolados		
-		for(i = 0; i < srcw ; i++)
+		for(i = 0; i < srch ; i++)
 		{
-			for(j = 0; j < srch  ; j++)
-			{	
-					RGBA* p_src = (RGBA*) &src_matriz[i][j*4];
-					RGBA* p_dst = (RGBA*) &dst_matriz[2*i][(j*4)*2];
+			for(j = 0; j < srcw ; j++)
+			{
+				if(i < srch-1 && j < srcw-1)
+				{	
+					RGBA* pA_src = &matrix_src[srch-1 - i][j]; 
+					RGBA* pB_src = &matrix_src[srch-1 - i][j+1]; 
+					RGBA* pC_src = &matrix_src[srch-1 - (i+1)][j];
+					RGBA* pD_src = &matrix_src[srch-1 - (i+1)][j+1];
+					
+					
+					RGBA* pA_dst = &matrix_dst[(dsth-1) - 2*i][2*j];
+					RGBA* pAB_dst = &matrix_dst[(dsth-1) - 2*i][2*j + 1];
+					RGBA* pB_dst = &matrix_dst[(dsth-1) - 2*i][2*j + 2];
+					RGBA* pAC_dst = &matrix_dst[(dsth-1)- (2*i + 1)][2*j];
+					RGBA* pABCD_dst = &matrix_dst[(dsth-1) - (2*i + 1)][2*j + 1];
+					RGBA* pBD_dst = &matrix_dst[(dsth-1) -(2*i + 1)][2*j + 2];
+					RGBA* pC_dst = &matrix_dst[(dsth-1) - (2*i + 2)][2*j];
+					RGBA* pCD_dst = &matrix_dst[(dsth-1) - (2*i + 2)][2*j + 1];
+					RGBA* pD_dst = &matrix_dst[(dsth-1)-(2*i + 2)][2*j + 2];
+					
+					pA_dst->a = pA_src->a; 
+					pA_dst->r = pA_src->r; 
+					pA_dst->g = pA_src->g; 
+					pA_dst->b = pA_src->b; 
+					
+					pB_dst->a = pB_src->a; 
+					pB_dst->r = pB_src->r; 
+					pB_dst->g = pB_src->g; 
+					pB_dst->b = pB_src->b;
+				
+					pC_dst->a = pC_src->a; 
+					pC_dst->r = pC_src->r; 
+					pC_dst->g = pC_src->g; 
+					pC_dst->b = pC_src->b;	
+						
+					pD_dst->a = pD_src->a; 
+					pD_dst->r = pD_src->r; 
+					pD_dst->g = pD_src->g; 
+					pD_dst->b = pD_src->b; 
+		
+					pAB_dst->a = suma2(pA_src->a, pB_src->a); 
+					pAB_dst->r = suma2(pA_src->r, pB_src->r); 
+					pAB_dst->g = suma2(pA_src->g, pB_src->g); 
+					pAB_dst->b = suma2(pA_src->b, pB_src->b); 
+			
+					pCD_dst->a = suma2(pC_src->a, pD_src->a); 
+					pCD_dst->r = suma2(pC_src->r, pD_src->r); 
+					pCD_dst->g = suma2(pC_src->g, pD_src->g); 
+					pCD_dst->b = suma2(pC_src->b, pD_src->b);
 
-					p_dst->r =	p_src->r;		
-					p_dst->g =	p_src->g;	
-					p_dst->b =	p_src->b;
+					pAC_dst->a = suma2(pA_src->a, pC_src->a); 
+					pAC_dst->r = suma2(pA_src->r, pC_src->r); 
+					pAC_dst->g = suma2(pA_src->g, pC_src->g); 
+					pAC_dst->b = suma2(pA_src->b, pC_src->b);
+				
+					pBD_dst->a = suma2(pB_src->a, pD_src->a); 
+					pBD_dst->r = suma2(pB_src->r, pD_src->r); 
+					pBD_dst->g = suma2(pB_src->g, pD_src->g); 
+					pBD_dst->b = suma2(pB_src->b, pD_src->b);
+					
+					pABCD_dst->a = suma4(pA_src->a, pB_src->a, pC_src->a, pD_src->a); 
+					pABCD_dst->r = suma4(pA_src->r, pB_src->r, pC_src->r, pD_src->r); 
+					pABCD_dst->g = suma4(pA_src->g, pB_src->g, pC_src->g, pD_src->g); 
+					pABCD_dst->b = suma4(pA_src->b, pB_src->b, pC_src->b, pD_src->b);
+			
+				}
+				
+				else if(j == srcw-1)
+				{
+					RGBA* p1 = (RGBA*) &matrix_dst[(dsth-1)-2*i][dstw-1];
+					RGBA* p2 = (RGBA*) &matrix_dst[(dsth-1)-(2*i+1)][dstw-1];
+					
+					RGBA* pd1 = (RGBA*) &matrix_dst[(dsth-1)-2*i][dstw-2];
+					RGBA* pd2 = (RGBA*) &matrix_dst[(dsth-1)-(2*i+1)][dstw-2];
+					
+					p1->a = pd1->a;
+					p1->g = pd1->g;
+					p1->b = pd1->b;
+					p1->r = pd1->r;
+					
+					p2->a = pd2->a;
+					p2->g = pd2->g;
+					p2->b = pd2->b;
+					p2->r = pd2->r;
+				}
+				else if(i == srch-1)
+				{
+					RGBA* p1 = (RGBA*) &matrix_dst[0][2*j];
+					RGBA* p2 = (RGBA*) &matrix_dst[0][2*j+1];
+					RGBA* p3 = (RGBA*) &matrix_dst[0][2*j+2];
+					
+					RGBA* pd1 = (RGBA*) &matrix_dst[1][2*j];
+					RGBA* pd2 = (RGBA*) &matrix_dst[1][2*j+1];
+					RGBA* pd3 = (RGBA*) &matrix_dst[1][2*j+2];
+					
+					p1->a = pd1->a;
+					p1->g = pd1->g;
+					p1->b = pd1->b;
+					p1->r = pd1->r;
+					
+					p2->a = pd2->a;
+					p2->g = pd2->g;
+					p2->b = pd2->b;
+					p2->r = pd2->r;
+					
+					p3->a = pd3->a;
+					p3->g = pd3->g;
+					p3->b = pd3->b;
+					p3->r = pd3->r;
 
+				}			
 			}
 
 		}
-					
-short val_b, val_g, val_r;
- //ahora calculamos los pixeles a interpolar
-		
-		for(i=0; i< srcw * 2 ;i++)
-		{
-			for(j=0; j< srch * 2 ;j++)
-			{
-					// si la fila es par y la columna impar ---- INTERPOLACIÓN HORIZONTAL
-
-				val_b = 0;
-				val_g = 0;  
-				val_r = 0;
-						
-					if ((i%2 == 0) && (j%2 == 1))
-						{
-						 RGBA* p_dst_atras = (RGBA*) &dst_matriz[i][(j-1)*4];
-						 RGBA* p_dst_adelante = (RGBA*) &dst_matriz[i][(j+1)*4];
-						 RGBA* p_dst = (RGBA*) &dst_matriz[i][j*4];
-						 
-						 val_b = (short)(p_dst_atras->b) + (short)(p_dst_adelante->b);
-						 val_b = val_b/2;
-						 
-						 val_g = (short)(p_dst_atras->g) + (short)(p_dst_adelante->g);
-						 val_g = val_g/2;
-						  
-						 val_r = (short)(p_dst_atras->r) + (short)(p_dst_adelante->r);
-						 val_r = val_r/2;
-						 
-						 p_dst->b = saturation((uint8_t)val_b, 255);
-						 p_dst->g = saturation((uint8_t)val_g, 255);
-						 p_dst->r = saturation((uint8_t)val_r, 255); 
-						}else{
-							// si la fila es impar y la columna es par
-								if((i%2 == 1) && (j%2 == 0)){
-										 RGBA* p_dst_abajo = (RGBA*) &dst_matriz[i-1][j*4];
-										 RGBA* p_dst_arriba = (RGBA*) &dst_matriz[i+1][j*4];
-										 RGBA* p_dst = (RGBA*) &dst_matriz[i][j*4];
-										 
-										 val_b = (short)(p_dst_abajo->b) + (short)(p_dst_arriba->b);
-										 val_b = val_b/2;
-										 
-										 val_g = (short)(p_dst_abajo->g) + (short)(p_dst_arriba->g);
-										 val_g = val_g/2;
-										  
-										 val_r = (short)(p_dst_abajo->r) + (short)(p_dst_arriba->r);
-										 val_r = val_r/2;
-										 
-										 p_dst->b = saturation((uint8_t)val_b, 255);
-										 p_dst->g = saturation((uint8_t)val_g, 255);
-										 p_dst->r = saturation((uint8_t)val_r, 255); 
-																					
-										} else {
-											// caso que estemos en una columna impar y fila impar
-											if((i%2 == 1) && (j % 2 == 1)){
-												 RGBA* p_dst_arriba_izq = (RGBA*) &dst_matriz[i+1][(j-1)*4];
-												 RGBA* p_dst_arriba_der = (RGBA*) &dst_matriz[i+1][(j+1)*4];
-												 RGBA* p_dst_abajo_izq = (RGBA*) &dst_matriz[i-1][(j-1)*4];
-												 RGBA* p_dst_abajo_der = (RGBA*) &dst_matriz[i-1][(j+1)*4];
-												 
-												 RGBA* p_dst = (RGBA*) &dst_matriz[i][j*4];
-												 
-												 val_b = (short)(p_dst_arriba_izq->b) + (short)(p_dst_arriba_der->b) + (short)(p_dst_abajo_izq->b) + (short)(p_dst_abajo_der->b) ;
-												 val_b = val_b/4;
-												 
-												 val_g = (short)(p_dst_arriba_izq->g) + (short)(p_dst_arriba_der->g) + (short)(p_dst_abajo_izq->g) + (short)(p_dst_abajo_der->g) ;
-												 val_g = val_g/4;
-												  
-												 val_r = (short)(p_dst_arriba_izq->r) + (short)(p_dst_arriba_der->r) + (short)(p_dst_abajo_izq->r) + (short)(p_dst_abajo_der->r) ;
-												 val_r = val_r/4;
-												 
-												 p_dst->b = saturation((uint8_t)val_b, 255);
-												 p_dst->g = saturation((uint8_t)val_g, 255);
-												 p_dst->r = saturation((uint8_t)val_r, 255); 
-												}								
-										} 
-							
-							
-							
-							} 
-							
-				
-				
-								
-				}
-
-
-		}
-
-  gettimeofday(&t_fin, NULL);
-
-  secs = timeval_diff(&t_fin, &t_ini);
-  printf("%.16g microsegundos\n", secs * 1000.0);				//MEDIDA EN MICRO SEGUNDOS 
-	
 }
+
+
+uint8_t suma2(uint8_t pa, uint8_t pb)
+{
+	uint16_t a = pa;
+	uint16_t b = pb;
+	uint8_t res = (a + b)/2;
+	
+	return res;
+}
+uint8_t suma4(uint8_t pa, uint8_t pb, uint8_t pc, uint8_t pd)
+{
+	uint16_t a = pa;
+	uint16_t b = pb;
+	uint16_t c = pc;
+	uint16_t d = pd;
+	uint8_t res = (a + b + c + d)/4;
+	
+	return res;
+}
+
