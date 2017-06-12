addb8629f8e6b67ecda8db30046b6617346fc0f3
franketi01 <francogm01@gmail.com>
2017-06-11 21:08:58 -0300

    Add files via upload
    ultimo convert

--- TP2/entregable/tp2-bundle.v2/src/filters/ASM_convert.asm
+++ TP2/entregable/tp2-bundle.v2/src/filters/ASM_convert.asm
@@ -6,30 +6,32 @@ extern C_convertRGBtoYUV
 
 section .data
 	
-	m_R: dd 1.0, 112.0, -38.0, 66.0
+	m_R: dd 1, 112, -38, 66
 	
-	m_G: dd 0.0, -94.0, -74.0, 129.0
+	m_G: dd 0, -94, -74, 129
 	
-	m_B: dd 0.0, -18.0, 112.0, 25.0
+	m_B: dd 0, -18, 112, 25
 	
-	aux1: dd 0.0, 128.0, 128.0, 128.0
+	aux1: dd 0, 128, 128, 128
 		
-	aux2: dd 1.0, 256.0, 256.0, 256.0
+	aux2: dd 1, 256, 256, 256
 	
 	aux3: dd 0, 128, 128, 16
 	
-	s_16: dd 0.0, 16.0, 16.0, 16.0
+	s_16: dd 0, 16, 16, 16
 	
-	s_128a: dd 0.0, 128.0, 128.0, 0
+	s_128a: dd 0, 128, 128, 0
 	
-	s_128b: dd 0.0, 0.0, 128.0, 128.0
+	s_128b: dd 0, 0, 128, 128
 	
-	m_Y: dd 1.0, 298.0, 298.0, 298.0
+	m_Y: dd 1, 298, 298, 298
 	
-	m_U: dd 0.0, 516.0, -100.0, 0.0
+	m_U: dd 0, 516, -100, 0
 	
-	m_V: dd 0.0, 0.0, -208.0, 409.0
-		
+	m_V: dd 0, 0, -208, 409
+	
+	s_255: dd 0, 255, 255, 255 
+
 section .text
 
 
@@ -74,31 +76,28 @@ ASM_convertYUVtoRGB:			;RDI = src, ESI = srcw, EDX = srch
 	punpcklbw xmm1, xmm0				;xmm1 = | 0 | 0 | 0 | 0 | ext0 Y | ext0 U | ext0 V | ext0 A |
 	punpcklwd xmm1, xmm0				;xmm1 = | ext Y | ext U | ext V | ext A |
 		
-	cvtdq2ps xmm1, xmm1                 ;xmm1 = conversion a float
 	
 	pshufd xmm2, xmm1, 0xa8				;xmm2 = |  U  |  U  |  U  |  A  |
 	pshufd xmm3, xmm1, 0x54				;xmm3 = |  V  |  V  |  V  |  A  |
 	pshufd xmm1, xmm1, 0xfc				;xmm1 = |  Y  |  Y  |  Y  |  A  |
 	
-	subps xmm1, xmm10					;xmm1 = | Y-16 | Y-16 | Y-16 |  A  |
-	subps xmm2, xmm11					;xmm2 = |   U  | U-16 | U-16 |  A  |
-	subps xmm3, xmm12					;xmm3 = | V-128| V-128|   V  |  A  |
+	psubd xmm1, xmm10					;xmm1 = | Y-16 | Y-16 | Y-16 |  A  |
+	psubd xmm2, xmm11					;xmm2 = |   U  | U-128| U-128|  A  |
+	psubd xmm3, xmm12					;xmm3 = | V-128| V-128|   V  |  A  |
 	
-	mulps xmm1, xmm13					;xmm1 = |298*(Y-16) | 298*(Y-16) | 298*(Y-16)|   A   |
-	mulps xmm2, xmm14					;xmm2 = |     0     |-100*(U-128)|516*(U-128)|   0   |
-	mulps xmm3, xmm15					;xmm3 = |409*(V-128)|-208*(V-128)|     0     |   0   |
+	pmulld xmm1, xmm13					;xmm1 = |298*(Y-16) | 298*(Y-16) | 298*(Y-16)|   A   |
+	pmulld xmm2, xmm14					;xmm2 = |     0     |-100*(U-128)|516*(U-128)|   0   |
+	pmulld xmm3, xmm15					;xmm3 = |409*(V-128)|-208*(V-128)|     0     |   0   |
 	
 	
-	addps xmm1, xmm2
-	addps xmm1, xmm3
+	paddd xmm1, xmm2
+	paddd xmm1, xmm3
 	
-	addps xmm1, xmm8
+	paddd xmm1, xmm8
 
-	divps xmm1, xmm9					;xmm1 = |  >> 8  |  >> 8  |  >> 8  |   A   |
-	
-	cvtps2dq xmm1, xmm1
-	
-	packusdw xmm1, xmm0					
+	psrad xmm1, 8
+
+	packssdw xmm1, xmm0					
 	packuswb xmm1, xmm0
 	
 	movd [rsi], xmm1
@@ -153,27 +152,25 @@ ASM_convertRGBtoYUV:				;RDI = src, ESI = srcw, EDX = srch
 	punpcklbw xmm1, xmm0				;xmm1 = | 0 | 0 | 0 | 0 | ext0 R | ext0 G | ext0 B | ext0 A |
 	punpcklwd xmm1, xmm0				;xmm1 = | ext R | ext G | ext B | ext A |
 	
-	cvtdq2ps xmm1, xmm1                 ;xmm1 = conversion a float
 	
 	pshufd xmm2, xmm1, 0xa8				;xmm2 = |  G  |  G  |  G  |  A  |
 	pshufd xmm3, xmm1, 0x54				;xmm3 = |  B  |  B  |  B  |  A  |
 	pshufd xmm1, xmm1, 0xfc				;xmm1 = |  R  |  R  |  R  |  A  |
 	
-	mulps xmm1, xmm13					;xmm1 = | 66*R | -38*R | 112*R |  A  |
-	mulps xmm2, xmm14					;xmm2 = | 129*G | -74*G | -94*G |  0 |
-	mulps xmm3, xmm15					;xmm3 = | 25*B | 112*B | -18*B |  0  |
+	pmulld xmm1, xmm13					;xmm1 = | 66*R | -38*R | 112*R |  A  |
+	pmulld xmm2, xmm14					;xmm2 = | 129*G | -74*G | -94*G |  0 |
+	pmulld xmm3, xmm15					;xmm3 = | 25*B | 112*B | -18*B |  0  |
 	
-	addps xmm1, xmm2					;xmm1 = | R + G | R + G | R + G |  A  |
-	addps xmm1, xmm3					;xmm1 = |  R+G+B | R+G+B | R+G+B |  A  |  
+	paddd xmm1, xmm2					;xmm1 = | R + G | R + G | R + G |  A  |
+	paddd xmm1, xmm3					;xmm1 = |  R+G+B | R+G+B | R+G+B |  A  |  
 	
-	addps xmm1, xmm12					;xmm1 = | ++128 |  ++128 | ++128 | A |
-	divps xmm1, xmm11					;xmm1 = | >> 8| >> 8| >> 8 | >> 8|
-	
+	paddd xmm1, xmm12					;xmm1 = | ++128 |  ++128 | ++128 | A |
 
-	cvtps2dq xmm1, xmm1
+	psrad xmm1, 8
+
 	paddd xmm1, xmm10
 	
-	packusdw xmm1, xmm0					;xmm1 = | 0 | 0 | 0 | 0 | Y | U | V | A | 
+	packssdw xmm1, xmm0					;xmm1 = | 0 | 0 | 0 | 0 | Y | U | V | A | 
 	packuswb xmm1, xmm0
 	
 	;paddb xmm1, xmm10
