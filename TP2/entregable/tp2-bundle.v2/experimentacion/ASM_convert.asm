d3e155e2f9e6925d4fa2a4fc29bec92414aadc8c
franketi01 <francogm01@gmail.com>
2017-06-12 13:02:58 -0300

    Create ASM_convert.asm
    con un px

--- TP2/entregable/tp2-bundle.v2/experimentacion/ASM_convert.asm
+++ TP2/entregable/tp2-bundle.v2/experimentacion/ASM_convert.asm
@@ -55,7 +55,7 @@ ASM_convertYUVtoRGB:			;RDI = src, ESI = srcw, EDX = srch
 	mul r9d
 	
 	mov ecx, eax
-	shr ecx, 1
+	;shr ecx, 1
 	pxor xmm0, xmm0
 	
 	movups xmm9, [m_128]
@@ -72,61 +72,41 @@ ASM_convertYUVtoRGB:			;RDI = src, ESI = srcw, EDX = srch
 	cmp ecx, 0			; de forma vertical. Hace las operaciones anteriores de forma horizontal, literalmente como se ve
 	je .fin 			; en el enunciado. Como es esta version me salto la parte de reacomodar (usar pshufd)
 						; deberia ser un poco mas optima
-	movq xmm1, [rdi]
+	movd xmm1, [rdi]
 	
 	punpcklbw xmm1, xmm0				;xmm1 = | 0 | 0 | 0 | 0 | ext0 Y | ext0 U | ext0 V | ext0 A |
 	
-	movups xmm4, xmm1
-	
-	punpckhwd xmm4, xmm0
 	punpcklwd xmm1, xmm0				;xmm1 = | ext Y | ext U | ext V | ext A |
 		
 	movups xmm2, xmm1 					;xmm2 = xmm1
 	movups xmm3, xmm1 					;xmm3 = xmm1 
 
-	movups xmm5, xmm4
-	movups xmm6, xmm4
-
 	psubd xmm1, xmm15
 	psubd xmm2, xmm14
 	psubd xmm3, xmm13
-
-	psubd xmm4, xmm15
-	psubd xmm5, xmm14
- 	psubd xmm6, xmm13
-
+	
 	pmulld xmm1, xmm12
 	pmulld xmm2, xmm11
 	pmulld xmm3, xmm10
 
-	pmulld xmm4, xmm12
-	pmulld xmm5, xmm11
-	pmulld xmm6, xmm10 
-
+	
 	phaddd xmm1, xmm0
 	pslldq xmm1, 8
 	phaddd xmm2, xmm3
 	phaddd xmm1, xmm2
 
-	phaddd xmm4, xmm0
-	pslldq xmm4, 8
-	phaddd xmm5, xmm6
-	phaddd xmm4, xmm5
-
 
 	paddd xmm1, xmm9
-	paddd xmm4, xmm9
 
 	psrad xmm1, 8	
-	psrad xmm4, 8
 
-	packssdw xmm1, xmm4					
+	packssdw xmm1, xmm0					
 	packuswb xmm1, xmm0
 	
-	movq [rsi], xmm1
+	movd [rsi], xmm1
 	
-	add rdi, 8
-	add rsi, 8
+	add rdi, 4
+	add rsi, 4
 	
 	dec ecx
 	jmp .ciclo
@@ -159,7 +139,7 @@ ASM_convertRGBtoYUV:				;RDI = src, ESI = srcw, EDX = srch
 	mul r9d
 	
 	mov ecx, eax
-	shr ecx, 1
+	;shr ecx, 1
 	pxor xmm0, xmm0
 
 	movups xmm12, [m_128] 	
@@ -173,57 +153,39 @@ ASM_convertRGBtoYUV:				;RDI = src, ESI = srcw, EDX = srch
 	cmp ecx, 0
 	je .fin
 
-	movq xmm1, [rdi]
+	movd xmm1, [rdi]
 	
 	punpcklbw xmm1, xmm0				;xmm1 = | extR1 | extG1 | extB1 | extA1 | extR0 | extG0 | extB0 | extA0 |
-	
-	movups xmm5, xmm1
-	
 	punpcklwd xmm1, xmm0				;xmm1 = | ext R | ext G | ext B | ext A |
-	punpckhwd xmm5, xmm0
 
 	movups xmm2, xmm1					;xmm2 = xmm1
 	movups xmm3, xmm1					;xmm3 = xmm1
 
-	movups xmm6, xmm5
-    movups xmm7, xmm5
 	
 	pmulld xmm3, xmm13					;xmm1 = | 66*R  | 129*G |  25*B |  0  |
 	pmulld xmm2, xmm14					;xmm2 =	|-38*R  | -74*G | 112*B |  0  |
 	pmulld xmm1, xmm15 					;xmm3 = |112*R  | -94*G | -18*B |  0  |
 	
-	pmulld xmm7, xmm13
-	pmulld xmm6, xmm14
-	pmulld xmm5, xmm15
-
 	phaddd xmm2, xmm3					;xmm2 = | R3+G3 |   B3  | R2+G2 | B2  |
 	phaddd xmm1, xmm0					;xmm1 = |   0   |   0   | R1+G1 | B1  |
 	pslldq xmm1, 8						;xmm1 = | R1+G1 |   B1  |   0   |  0  |
 	phaddd xmm1, xmm2					;xmm1 = |R3+G3+B3|R2+G2+B2|R1+G1+B1|   0  |
 
-	phaddd xmm6, xmm7
-	phaddd xmm5, xmm0
-	pslldq xmm5, 8
-	phaddd xmm5, xmm6
-
 	paddd xmm1, xmm12					;xmm1 = | + 128 | + 128 | + 128 |  0  |	
-	paddd xmm5, xmm12
 
 	psrad xmm1, 8						;xmm1 = | >> 8  | >> 8  | >> 8  |  0  |
-	psrad xmm5, 8
 
 	paddd xmm1, xmm11					;xmm1 = | + 16  | + 128 | + 128 |  0  |
-	paddd xmm5, xmm11
 
-	packssdw xmm1, xmm5					;xmm1 = | 0 | 0 | 0 | 0 | Y | U | V | A | 
+	packssdw xmm1, xmm0					;xmm1 = | 0 | 0 | 0 | 0 | Y | U | V | A | 
 	packuswb xmm1, xmm0
 	
 	;paddb xmm1, xmm10
 	
-	movq [rsi], xmm1
+	movd [rsi], xmm1
 	
-	add rdi, 8
-	add rsi, 8
+	add rdi, 4
+	add rsi, 4
 	
 	dec ecx
 	
